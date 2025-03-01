d1f3b3481a06185a51ebf1da9157bc61261bd9ea
BlueKitsune狐 <116144344+bluekitsune-sad@users.noreply.github.com>
2025-03-01 19:07:48 +0500

    Update mainMenu.js

--- src/scenes/mainMenu.js
+++ src/scenes/mainMenu.js
@@ -10,7 +10,7 @@ export default function mainMenu() {
     k.add([k.sprite("chemical-bg"), k.pos(0, 0), k.scale(2), k.opacity(0.8)]),
     k.add([
       k.sprite("chemical-bg"),
-      k.pos(1920, 0),
+      k.pos(bgPieceWidth * 2, 0),
       k.scale(2),
       k.opacity(0.8),
     ]),
